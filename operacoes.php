bde146afb864d9f43b24898d7d21cff2e04a3551
Everson Carvalho da Silva <everson_silva2009@hotmail.com>
2015-03-05 11:31:10 -0300

    Fim

---
+++ operacoes.php
@@ -0,0 +1,22 @@
+<?php
+
+function soma($x, $y) {
+    $soma = $x + $y;
+    return $soma;
+}
+
+function subtracao($x, $y) {
+    $subtracao = $x - $y;
+    return $subtracao;
+}
+
+function multiplicacao($x,$y){
+    $multiplicacao = $x * $y;
+    return $multiplicacao;
+}
+
+function divisao($x,$y){
+    $divisao = $x/$y;
+    return $divisao;
+}
+?>
