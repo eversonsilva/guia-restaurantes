16dcef0df91bea5ba60c99625eeb997ea81b2a6c
Everson Carvalho da Silva <everson_silva2009@hotmail.com>
2015-03-05 11:31:22 -0300

    Fim

---
+++ teste.php
@@ -0,0 +1,23 @@
+<?php include "operacoes.php";
+
+$x = 7;
+$y = 6;
+
+$soma = soma($x, $y);
+echo "O resultado : " .$soma;
+echo "<br>";
+echo "<br>";
+
+$subtracao = subtracao($x, $y);
+echo "O resultado : " .$subtracao;
+echo "<br>";
+echo "<br>";
+
+$multiplicacao = multiplicacao($x, $y);
+echo "O resultado : " .$multiplicacao;
+echo "<br>";
+echo "<br>";
+
+$divisao = divisao($x, $y);
+echo "O resultado : " .$divisao;
+?>
